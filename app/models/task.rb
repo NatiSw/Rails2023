fe6aaadb60250cf6142a7f51a8db4e33f9ffeac0
NatiSw <137528277+NatiSw@users.noreply.github.com>
2023-06-24 17:06:58 +0000

    user z taskami

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
 validates :name, length: { minimum: 5 }
 validates :description, length: { maximum: 5 }
+
+belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_24_155525) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_163013) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
